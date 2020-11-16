764cfc3a8741c08bb8200488f7ba40372c76bb45
Alonso Anchante Moreno <alonso.amoreno@live.com>
2020-11-16 11:57:52 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import Clima from './components/Clima'
+import Error from './components/Error'
+import Formulario from './components/Formulario'
+import Header from './components/Header'
 
 function App() {
+  // State del formulacio
+  const [busqueda, setBusqueda] = useState({
+    ciudad: '',
+    pais: '',
+  })
+
+  const [consultar, setConsultar] = useState(false)
+
+  const [error, setError] = useState(false)
+
+  // Extraer datos
+  const { ciudad, pais } = busqueda
+
+  // Pasar valores de la API al state
+  const [resultado, setResultado] = useState({})
+
+  // crear el useEffect
+  useEffect(() => {
+    // consultar la API
+    const consultarAPI = async () => {
+      // http://api.openweathermap.org/data/2.5/weather?q=pisco,peru&appid=adb1d16777c24e5abe22a38ee9363518  --> De esta forma se llama a la API
+
+      if (consultar) {
+        // API Key
+        const appID = 'adb1d16777c24e5abe22a38ee9363518'
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appID}`
+
+        const respuesta = await fetch(url)
+        const resultado = await respuesta.json()
+
+        setResultado(resultado)
+        setConsultar(false)
+
+        // Detecta si hubo resutados correctos en la consulta
+        if (resultado.cod === '404') {
+          setError(true)
+        } else {
+          setError(false)
+        }
+      }
+    }
+
+    consultarAPI()
+  }, [consultar])
+
+  let componente
+  if (error) {
+    componente = <Error mensaje="No hay resultados" />
+  } else {
+    componente = <Clima resultado={resultado} />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header titulo="Clima React App" />
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario
+                busqueda={busqueda}
+                setBusqueda={setBusqueda}
+                setConsultar={setConsultar}
+              />
+            </div>
+            <div className="col m6 s12">{componente}</div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
